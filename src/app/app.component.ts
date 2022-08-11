822d44e2376d23e00cdc56f60d9fe6f2a293c881
gurinder <gurinder.singh@mjsiver.com>
2022-08-11 20:52:31 +0530

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Template Driven Form Tutorial';
-  AddEmployees(item:any)
+  firstName? : string;
+  EmailID? : string
+  Mobile? : string
+
+
+  PrefilledData(EmployeeForm:NgForm)
   {
-    console.warn(item);
+    // EmployeeForm.value.EmpName = "Tech Engineer";
+    // EmployeeForm.value.EmpAddress = "Tech Engineer";
+
+    // EmployeeForm.setValue({
+    //     PersonalData : {
+    //              EmpName: "Tech Engineer",
+    //              EmpAddress: "developer@94.com",
+    //              EmpMobile: "8544884846"
+    //     } 
+    // })
+    
+    EmployeeForm.form.patchValue({
+      PersonalData : {
+                     EmpName: "Tech Engineer",
+                     EmpMobile: "8544884846"
+            } 
+    })
+
+  }
+
+  AddEmployees(EmployeeForm:NgForm)
+  {
+    console.warn(EmployeeForm.value);
+    this.firstName = EmployeeForm.value.PersonalData.EmpName;
+    this.EmailID = EmployeeForm.value.PersonalData.EmpAddress;
+    this.Mobile = EmployeeForm.value.PersonalData.EmpMobile;
+  }
+
+  ClearForm(EmployeeForm:NgForm)
+  {
+   EmployeeForm.form.reset();
   }
 }
